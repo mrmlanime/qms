74d0133becf23df9c6086ef4ea9909000c54fc89
rodel <rodel@rodel.manila.orangeandbronze.com>
2013-02-05 16:03:35 +0800

    Delete Unused Files

--- src/test/java/com/onb/qms/domain/VisitorDataOnDemand.java
+++
@@ -1,9 +0,0 @@
-package com.onb.qms.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-import com.torm.domain.Visitor;
-
-@RooDataOnDemand(entity = Visitor.class)
-public class VisitorDataOnDemand {
-}
