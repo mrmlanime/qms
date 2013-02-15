9c4abc1568517d7b6f150a4fde3ac437844e5a33
rodel <rodel@rodel.manila.orangeandbronze.com>
2013-02-15 16:08:29 +0800

    adding model to display the status of all staff

--- src/main/java/com/torm/controller/AdminController.java
+++ src/main/java/com/torm/controller/AdminController.java
@@ -1,13 +1,17 @@
 package com.torm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.torm.domain.Staff;
 
 @Controller
 public class AdminController {
 
-	@RequestMapping(value="/admin")
-	public String display(){
-		return null;
+	@RequestMapping(value="/admin/home")
+	public String display(Model model){
+		model.addAttribute("staffs", Staff.findAllStaffs());
+		return "admin/home";
 	}
 }
